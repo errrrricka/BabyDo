7c612a87b8e99f6eb34c371453387a6ea96b80d8
Ericka Emery <ericka@elonandcompany.com>
2017-08-28 20:58:59 -0500

    initial commit

---
+++ babydo/src/Slideshow.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Slideshow() {
+  return (
+    <div>
+    <button className="startShow">Click to enable Slideshow</button>
+    <br></br>
+    <button className="Next">Click to See Next Slideshow</button>
+    <br></br>
+    <button className="Home">Click to return Home</button>
+    </div>
+  );
+}
+
+export default Slideshow;
